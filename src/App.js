72b14ce4d149c078f3e703878131ef279b4919fe
Bizimungu Pascal <bizip@DESKTOP-TA09OM0.localdomain>
2023-02-02 12:11:38 +0200

    Display Popup on click

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import { useState } from 'react';
+import { useState, useId } from 'react';
 import './App.css';
 import Question from './Question';
 
 function App() {
   const [data, setData] = useState({});
   const [result, setResult] = useState([]);
+  const [isOpen,setIsOpen] =useState(false);
+  const id = useId();
   const handleSubmit = (e) => {
     e.preventDefault();
     setResult(data.split(/(\s+)/).filter((e) => e.trim().length > 0));
+  };
+
+  const handleOption = (e) => {
+    setIsOpen((current) => !current);
   };
 
   return (
@@ -19,12 +25,21 @@ function App() {
       </form>
 
       <ul>
-        {result.length > 0 && result.map((item, index) => (
-          <li key={index + 1}>
-            <a href="#">{item}</a>
+        {result.length > 0 && result.map((item) => (
+          <li key={id}>
+            <button id={id} type="button" onClick={(e) => { handleOption(e); }}>{item}</button>
           </li>
         ))}
       </ul>
+
+
+      {isOpen && 
+        <ul>
+        <li>Create Drop down</li>
+        <li>Create List</li>
+        <li>Create Radio</li>
+      </ul>
+      }
     </div>
   );
 }
