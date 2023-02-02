99176008ee4f60b4f5c7f42dc43e68311cec5c27
Mundurujoana <mundujoana@gmail.com>
2023-02-02 10:24:48 +0200

    added a question component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
+import Question from './Question';
 
 function App() {
   return (
     <div className="App">
-      <h1>Questions editor</h1>
+      {/* <h1>Questions editor</h1>
       <form>
         <textarea>Some text...</textarea>
-      </form>
+      </form> */}
+      <Question />
     </div>
   );
 }
