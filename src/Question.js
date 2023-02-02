26787c22aa1d95a175025ba7c16d278f4d7060af
Natete-Verite <nateteverite@gmail.com>
2023-02-02 12:19:27 +0200

    add options on each word

--- src/Question.js
+++ src/Question.js
@@ -1,43 +1,43 @@
-import React, { useState } from 'react';
+// import React, { useState } from 'react';
 
-const Question = () => {
-  const [selectedWord, setSelectedWord] = useState('');
-  const [showAlternatives, setShowAlternatives] = useState(false);
+// const Question = () => {
+//   const [selectedWord, setSelectedWord] = useState('');
+//   const [showAlternatives, setShowAlternatives] = useState(false);
 
-  const handleClick = (word) => {
-    setSelectedWord(word);
-    setShowAlternatives(!showAlternatives);
-  };
+//   const handleClick = (word) => {
+//     setSelectedWord(word);
+//     setShowAlternatives(!showAlternatives);
+//   };
 
-  const words = [
-    { word: 'React', alternatives: ['Vue', 'Angular', 'Ember'] },
-    { word: 'JavaScript', alternatives: ['TypeScript', 'CoffeeScript', 'Elm'] },
-    { word: 'State', alternatives: ['Props', 'Context', 'Redux'] },
-  ];
+//   const words = [
+//     { word: 'React', alternatives: ['Vue', 'Angular', 'Ember'] },
+//     { word: 'JavaScript', alternatives: ['TypeScript', 'CoffeeScript', 'Elm'] },
+//     { word: 'State', alternatives: ['Props', 'Context', 'Redux'] },
+//   ];
 
-  return (
-    <div className="question">
-      {words.map((wordData, index) => {
-        const { word, alternatives } = wordData;
-        return (
-          <div key={index} className="word">
-            <span onClick={() => handleClick(word)} className={selectedWord === word ? 'active' : ''}>
-              {selectedWord === word ? alternatives[0] : word}
-            </span>
-            {selectedWord === word && showAlternatives && (
-              <ul className="alternatives">
-                {alternatives.map((alt, i) => (
-                  <li key={i} onClick={() => handleClick(alt)}>
-                    {alt}
-                  </li>
-                ))}
-              </ul>
-            )}
-          </div>
-        );
-      })}
-    </div>
-  );
-};
+//   return (
+//     <div className="question">
+//       {words.map((wordData, index) => {
+//         const { word, alternatives } = wordData;
+//         return (
+//           <div key={index} className="word">
+//             <span onClick={() => handleClick(word)} className={selectedWord === word ? 'active' : ''}>
+//               {selectedWord === word ? alternatives[0] : word}
+//             </span>
+//             {selectedWord === word && showAlternatives && (
+//               <ul className="alternatives">
+//                 {alternatives.map((alt, i) => (
+//                   <li key={i} onClick={() => handleClick(alt)}>
+//                     {alt}
+//                   </li>
+//                 ))}
+//               </ul>
+//             )}
+//           </div>
+//         );
+//       })}
+//     </div>
+//   );
+// };
 
-export default Question;
+// export default Question;
