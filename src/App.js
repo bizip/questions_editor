bbdb13465f105d16464e185af4f2a6abb707353c
Natete-Verite <nateteverite@gmail.com>
2023-02-02 10:26:18 +0200

    Fixed the eslint error

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import './App.css';
 function App() {
   const handleSubmit = () => {
 
-  }
+  };
   return (
     <div className="App">
       <h1>Questions editor</h1>
       <form onSubmit={handleSubmit}>
-        <textarea></textarea>
-        <input type='submit' value="submit" />
+        <textarea />
+        <input type="submit" value="submit" />
       </form>
     </div>
   );
