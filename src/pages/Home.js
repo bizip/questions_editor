12508437b098af8e7fc655ee4e2136062cf53848
Bizimungu Pascal <bizip@DESKTOP-TA09OM0.localdomain>
2023-02-20 16:48:33 +0200

    Center a category div

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,11 +4,21 @@ import { Link } from 'react-router-dom';
 const Home = () => {
   return (
     <section className='landing__page'>
-      <div className='choice'>
-        <Link to='/clinicaltrials' className="choice__link">Clinical Trials</Link>
-        <Link to='/regulatoryafairs' className="choice__link">Regulatory Afairs</Link>
-        <Link to='/pharmacovigilance' className="choice__link">Pharmacov Igilance</Link>
+      <nav className='top_nav'>
+        <Link to='/'><img src='https://media.licdn.com/dms/image/C560BAQGghHrBNT18Ww/company-logo_200_200/0/1524310864696?e=1684368000&v=beta&t=aD2xtdD4R2yH2bQrd8RRkveyW98oUzJzy7c0Ien2y-c' alt='logo' /></Link>
+
+        <div className='landing_title'>
+          <p>Clinical Trial generator</p>
+        </div>
+      </nav>
+      <div className='choice_container'>
+        <div className='choice'>
+          <Link to='/clinicaltrials' className="choice__link">Clinical Trials</Link>
+          <Link to='/regulatoryafairs' className="choice__link">Regulatory Afairs</Link>
+          <Link to='/pharmacovigilance' className="choice__link">Pharmacovigilance</Link>
+        </div>
       </div>
+
     </section>
   )
 }
